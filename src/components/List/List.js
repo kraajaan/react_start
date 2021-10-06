91094b1eac494d8bd899cbcd904f23f8602fc7c3
Mateusz Krajewski <admin@cray.lh.pl>
2021-10-06 21:54:12 +0200

    Interval commit in Submodule 15.5

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -52,7 +52,7 @@ class List extends React.Component {
         ))}
         </div>
         <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
         </div>
       </section>
     )
