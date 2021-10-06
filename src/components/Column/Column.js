0f204496bf14924207ee0ec428e7bcdf91c18d17
Mateusz Krajewski <admin@cray.lh.pl>
2021-10-06 22:23:29 +0200

    Completion of Submodule 15.5

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,6 +3,7 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
+import Icon from '../Icon/Icon.js';
 import {settings} from '../../data/dataStore.js';
 
 class Column extends React.Component {
@@ -31,7 +32,12 @@ class Column extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}>
+          {this.props.title}
+          <span className={styles.icon}>
+            <Icon name={this.props.icon} />
+          </span>
+        </h3>
         {this.state.cards.map(({key, ...cardProps}) => (
           <Card key={key} {...cardProps} />
         ))}
